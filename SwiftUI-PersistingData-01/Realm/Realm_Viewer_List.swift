1616d036eef66da290225d13628c9d63552ebf15
rjb888 <robert.j.bronson@gmail.com>
2021-05-05 14:29:16 -0700

    added cat object to second database

--- SwiftUI-PersistingData-01/Realm/Realm_Viewer_List.swift
+++ SwiftUI-PersistingData-01/Realm/Realm_Viewer_List.swift
@@ -10,61 +10,61 @@ import RealmSwift
 
 struct Realm_Viewer_List: View {
     
-    @State private var peopleResults: Results<Person>
-    
-    init() {
-        let realm = try! Realm()
-        peopleResults = realm.objects(Person.self)
-    }
-
-    func saveData() {
-        let fido = Dog()
-        fido.name = "Fido"
-        fido.age = 3
-        
-        let rex = Dog()
-        rex.name="Rex"
-        rex.age=4
-        
-        let protagonist = Person()
-        protagonist.name = "???"
-        protagonist.dogs.append(fido)
-        protagonist.dogs.append(rex)
-        
-        let realm = try! Realm()
-        
-        let people = realm.objects(Person.self)
-        protagonist.name = "Person #" + String(people.count + 1)
-        try! realm.write {
-            realm.add(protagonist)
-        }
-    }
-    
-    func loadData() -> Results<Person> {
-      
-        let realm = try! Realm()
-        let allPeeps = realm.objects(Person.self)
-        print(allPeeps.count)
-        return allPeeps
-    }
-    
+//    @State private var peopleResults: Results<Person>
+//
+//    init() {
+//        let realm = try! Realm()
+//        peopleResults = realm.objects(Person.self)
+//    }
+//
+//    func saveData() {
+//        let fido = Dog()
+//        fido.name = "Fido"
+//        fido.age = 3
+//
+//        let rex = Dog()
+//        rex.name="Rex"
+//        rex.age=4
+//
+//        let protagonist = Person()
+//        protagonist.name = "???"
+//        protagonist.dogs.append(fido)
+//        protagonist.dogs.append(rex)
+//
+//        let realm = try! Realm()
+//
+//        let people = realm.objects(Person.self)
+//        protagonist.name = "Person #" + String(people.count + 1)
+//        try! realm.write {
+//            realm.add(protagonist)
+//        }
+//    }
+//
+//    func loadData() -> Results<Person> {
+//
+//        let realm = try! Realm()
+//        let allPeeps = realm.objects(Person.self)
+//        print(allPeeps.count)
+//        return allPeeps
+//    }
+//
     var body: some View {
         VStack {
             Text("Realm Example")
-            HStack{
-                Button("Load", action: {
-                    peopleResults = loadData()
-                })
-                Spacer()
-                Button("Save", action: {
-                    saveData()
-                    peopleResults = loadData()
-                })
-            }
-            
-            ForEach(self.peopleResults.map(Person.init), id: \.self) { aPerson in
-                Text(aPerson.name)
-            }
+//            HStack{
+//                Button("Load", action: {
+//                    peopleResults = loadData()
+//                })
+//                Spacer()
+//                Button("Save", action: {
+//                    saveData()
+//                    peopleResults = loadData()
+//                })
+//            }
+//            
+//            ForEach(self.peopleResults.map(Person.init), id: \.self) { aPerson in
+//                Text(aPerson.name)
+//            }
             
         } .padding()
     }
